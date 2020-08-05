ea80aef512892fad7dfbc74dd413ab43aea26f2f
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-08-05 23:44:48 +0300

    Fetch, process and render summary statistics

--- analysis/views.py
+++ analysis/views.py
@@ -6,30 +6,35 @@ import json
 
 def summary(request):
     url = 'https://api.covid19api.com/summary'
-    json_data = urllib.request.urlopen(url)
-    data = json.load(json_data)
+    try:
+        json_data = urllib.request.urlopen(url)
+        data = json.load(json_data)
+        world = data['Global']
+        date = data['Date']
 
-    world = data['Global']
-    date = data['Date']
+        total_confirmed = world['TotalConfirmed']
+        total_recovered = world['TotalRecovered']
+        total_deaths = world['TotalDeaths']
+        new_confirmed = world['NewConfirmed']
+        new_recovered = world['NewRecovered']
+        new_deaths = world['NewDeaths']
 
-    total_confirmed = world['TotalConfirmed']
-    total_recovered = world['TotalRecovered']
-    total_deaths = world['TotalDeaths']
-    new_confirmed = world['NewConfirmed']
-    new_recovered = world['NewRecovered']
-    new_deaths = world['NewDeaths']
+        dictionary = {
+            'date':date,
+            'total_confirmed':total_confirmed, 
+            'total_recovered':total_recovered, 
+            'total_deaths':total_deaths,
+            'new_confirmed':new_confirmed, 
+            'new_recovered':new_recovered, 
+            'new_deaths':new_deaths
+        }
+        return dictionary
 
-    dictionary = {
-        'date':date,
-        'total_confirmed':total_confirmed, 
-        'total_recovered':total_recovered, 
-        'total_deaths':total_deaths,
-        'new_confirmed':new_confirmed, 
-        'new_recovered':new_recovered, 
-        'new_deaths':new_deaths
-    }
-    return dictionary
-
+    except Exception as e:
+        error = True
+        msg = f'{e}\n No Internert Connection'
+        return error, msg
+    
 def index(request):
     context = {
         'dict':summary(request)
