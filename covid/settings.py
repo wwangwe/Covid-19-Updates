028e2186cf50e2f95f5a1e4f1b25cfcd1aba9fea
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-08-08 06:35:45 +0300

    Correct deploy error static files--BASE_DIR

--- covid/settings.py
+++ covid/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
