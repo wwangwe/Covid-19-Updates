844b4724d8989d98acacfcecdc4651f541e551c8
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-08-11 16:32:36 +0300

    Update news feature

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from analysis.views import index, news
+from analysis.views import index, news, article
 
 app_name = 'analysis'
 
 urlpatterns = [
     path('', index, name='index'),
     path('news/', news, name='news'),
+    path('news/article/<str:url>/', article, name='article'),
 ]
