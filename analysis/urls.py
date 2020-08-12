cc01f9680cadb6871356d6c738543e6a9a809fd6
Timothy-Wangwe <wangwetymothy@gmail.com>
2020-08-12 22:46:44 +0300

    license page

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from analysis.views import index, news, article
+from analysis.views import index, news, article, license
 
 app_name = 'analysis'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('license/', license, name='license'),
     path('news/', news, name='news'),
     path('news/article/<str:url>/', article, name='article'),
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -43,6 +43,9 @@ def index(request):
     context = summary(request)
     return render(request, 'analysis/index.html', context)
 
+def license(request):
+    return render(request, 'analysis/license.html')
+
 def news(request):
     header = ({'User-Agent':
            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'})
